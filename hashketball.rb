88c7f9edeea43d6153ef20c2a408b29d2d76ebbe
Justin Hamlin <justin.hamlin@gmail.com>
2018-03-10 17:15:31 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,11 +130,9 @@ def game_hash
 def num_points_scored(name)
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
-        #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
-        
- 
-        #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
-        data.each do |data_item|
+      data.each do |stat, result|
+       binding.pry
+    
           
            
       end
