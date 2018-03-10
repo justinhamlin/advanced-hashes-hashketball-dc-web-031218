f11915e8ff83f7f98bb304189d191dc37b95509d
Justin Hamlin <justin.hamlin@gmail.com>
2018-03-10 16:59:57 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -137,7 +137,7 @@ def good_practices
  
         #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
         data.each do |data_item|
-          binding.pry
+          
            
       end
     
