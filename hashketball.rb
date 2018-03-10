4a676372528ac26225adf3b6a121c720ae8d6f88
Justin Hamlin <justin.hamlin@gmail.com>
2018-03-10 16:52:40 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,7 +130,7 @@ def game_hash
 def good_practices
   game_hash.each do |location, team_data|
     #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
-  
+    
       team_data.each do |attribute, data|
         #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
         binding.pry
